7d07f67b3c9975496ae8b12b15138a38fe2ffddd
hyx <heyuxiao20@mails.ucas.ac.cn>
2023-01-25 11:04:57 +0800

    修改了Trace类型

--- code/Trace.java
+++ code/Trace.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class TraceUnit {
     private final Literal literal;
     private final Clause clause;
@@ -13,17 +15,22 @@ class TraceUnit {
 }
 
 class Trace {
-    private TraceUnit[] traceUnits;
+    // TODO: use ArrayList instead
+    private ArrayList<TraceUnit> traceUnits;
 
-    Trace(TraceUnit[] traceUnits) {
+    Trace(ArrayList<TraceUnit> traceUnits) {
         this.traceUnits = traceUnits;
+    }
+
+    void addTraceUnit(TraceUnit traceUnit) {
+        this.traceUnits.add(traceUnit);
     }
 
     public String toString() {
         String traceString = "{";
-        for(int i = 0; i < this.traceUnits.length; i++) {
-            traceString += this.traceUnits[i].toString();
-            if(i != this.traceUnits.length - 1) {
+        for(int i = 0; i < this.traceUnits.size(); i++) {
+            traceString += this.traceUnits.get(i).toString();
+            if(i != this.traceUnits.size() - 1) {
                 traceString += ", ";
             }
         }
